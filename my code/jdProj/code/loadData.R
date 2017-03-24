e7b9a4b64db64496f025960181c8a2527d9574ed
she changlue <snakepointid@sina.cn>
2017-03-24 18:39:23 +0800

    update

---
+++ my code/jdProj/code/loadData.R
@@ -0,0 +1,8 @@
+maxrows<--1
+actdfrows<-10000000
+JData_Product <- read.csv("C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/jdProj/rawdata/JData_Product.csv", stringsAsFactors=FALSE,nrows=maxrows)
+JData_Comment <- read.csv("C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/jdProj/rawdata/JData_Comment.csv", stringsAsFactors=FALSE,nrows=maxrows)
+JData_User <- read.csv("C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/jdProj/rawdata/JData_User.csv", stringsAsFactors=FALSE,nrows=maxrows)
+JData_Action_201604 <- read.csv("C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/jdProj/rawdata/JData_Action_201604.csv", stringsAsFactors=FALSE,nrows=actdfrows)
+JData_Action_201603 <- read.csv("C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/jdProj/rawdata/JData_Action_201603.csv", stringsAsFactors=FALSE,nrows=actdfrows)
+JData_Action_201602 <- read.csv("C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/jdProj/rawdata/JData_Action_201602.csv", stringsAsFactors=FALSE,nrows=actdfrows)
