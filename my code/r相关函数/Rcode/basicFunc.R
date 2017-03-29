a28604189658748b35c9378ba786606c35ee828d
she changlue <snakepointid@sina.cn>
2017-03-29 19:14:16 +0800

    update

--- my code/r相关函数/Rcode/basicFunc.R
+++ my code/r相关函数/Rcode/basicFunc.R
@@ -3,8 +3,13 @@ ignoreLabel<-as.integer(-2)
 getlabel<-function(sku_id,type,cate){
   p<-which(type=='order'&cate==8)
   if(p%>%length>0){return(sku_id[p])}
-  p<-which(cate==8)
+  p<-which(cate==8|type=='order')
   if(p%>%length==0){return(neglabel)}
   return(ignoreLabel)
 }
  
+oneHotEncode<-function(vec){
+  hotcode<-diag(length(vec))%>%apply(.,1,function(x)paste(x,collapse="#"))
+  names(hotcode)<-vec
+  return(hotcode)
+}
--- my code/r相关函数/Rcode/loadDataPC.R
+++ my code/r相关函数/Rcode/loadDataPC.R
@@ -1,8 +1,8 @@
 library(data.table)
 library(dplyr)
 #read date
-source('C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/Rcode/basicFunc.R')
-maxrows=200000
+source('C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/r相关函数/Rcode/basicFunc.R')
+maxrows=3000000
 act02<-fread('C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/rawdata/JData_Action_201602.csv',nrows = maxrows)
 act03<-fread('C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/rawdata/JData_Action_201603.csv',nrows = maxrows)
 act03_extra<-fread('C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/rawdata/JData_Action_201603_extra.csv',nrows = maxrows)
@@ -16,8 +16,11 @@ gc()
 buytypeMP<-c("browse","buycar","cancel","order","star","hit")
 actionInfo[,'type']<-actionInfo[,buytypeMP[type]]
 #indexmap and save
+cateOneHot<-c(8,5,6,4,7,11,9,10)%>%oneHotEncode
+typeOneHot<-c("browse","buycar","cancel","order","star","hit")%>%oneHotEncode
+#indexmap and save
 buyedInfo<-actionInfo[type=="order",]
- 
+
 prodEmbIndx<-buyedInfo$sku_id%>%table%>%sort(.,T)
 prodEmbIndxMP<-data.table(mp=prodEmbIndx%>%names%>%as.integer,index=c(1:length(prodEmbIndx)))
 
@@ -33,7 +36,6 @@ write.csv(prodEmbIndxMP,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/proj
 write.csv(userEmbIndxMP,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/save/userEmbIndxMP.csv",row.names = F)
 write.csv(brandEmbIndxMP,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/save/brandEmbIndxMP.csv",row.names = F)
 write.csv(targProdEmbIndxMP,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/save/targProdEmbIndxMP.csv",row.names = F)
-
 #time map
 actionInfo[,"day"]<-actionInfo[,substr(time,1,10)]%>%as.character
 actionInfo[,"hour"]<-actionInfo[,substr(time,12,13)]%>%as.numeric
@@ -44,7 +46,7 @@ lastday<-as.Date("2016-04-15")
 dateSpan<-seq(earlday,lastday,1)
 dateSpan<-sort(dateSpan,decreasing = T)
 
-weekMP<-c(0:6)
+weekMP<-c("0#0#0#0#0#0","0#0#0#0#0#1","0#0#0#0#1#0","0#0#0#1#0#0","0#0#1#0#0#0","0#1#0#0#0#0","1#0#0#0#0#0")
 names(weekMP)<-c( "星期一" ,"星期二","星期三","星期四","星期五","星期六", "星期日" )
 weekday<-weekMP[dateSpan%>%weekdays]
 names(weekday)<-dateSpan%>%as.character 
@@ -55,7 +57,7 @@ windowP<-seq(1,vl,5)
 windowdayMP<-rep(dateSpan[windowP],each=5)[1:vl]
 names(windowdayMP)<-dateSpan%>%as.character 
 actionInfo[,"windowday"]<-actionInfo[,windowdayMP[day]]
- 
+
 names(dateSpan)<-dateSpan%>%as.character 
 actionInfo[,"day"]<-actionInfo[,dateSpan[day]]
 #whether buy
@@ -81,11 +83,40 @@ actionInfo<-actionInfo[,.(cate=first(cate),actimes=round(1/.N,3),brand=first(bra
                        .(user_id,windowday,historyBuyed,sku_id,type)]
 actionInfo[,'timegap']<-actionInfo[,ifelse(day==windowday,round(1/(60*(24-hour)-minute),3),0)]
 actionInfo<-actionInfo[order(user_id,windowday,-day,-hour,-minute),]
-actionInfo[,'act']<-actionInfo[,paste(user_id,weekday,type,cate,brand,sku_id,actimes,timegap,historyBuyed,sep="#")]
-actionInfo<-actionInfo[,.(action=paste(act[1:min(length(act),max_seq_length)],collapse = "<-")),.(user_id,windowday)]
+#data maping
+actionInfo[,'type']<-actionInfo$type%>%as.character
+actionInfo[,'cate']<-actionInfo$cate%>%as.character
+
+actionInfo[,'type']<-actionInfo[,typeOneHot[type]]
+actionInfo[,'cate']<-actionInfo[,cateOneHot[cate]]
+
+#get rawfeed
+actionInfo[,'static_feat']<-actionInfo[,paste(weekday,type,cate,actimes,timegap,historyBuyed,sep="#")]
+actionInfo<-actionInfo[,.(static_feat=paste(static_feat[1:min(length(static_feat),max_seq_length)],collapse = "<-"),
+                          seq_length=min(length(static_feat),max_seq_length),
+                          brandidx=paste(brand[1:min(length(static_feat),max_seq_length)],collapse = "<-"),
+                          skuidx=paste(sku_id[1:min(length(static_feat),max_seq_length)],collapse = "<-")),.(user_id,windowday)]
+
 #merge and save
 setkey(actionInfo,user_id,windowday)
 setkey(targInfo,user_id,windowday)
 finalDF<-actionInfo[targInfo]
-finalDF<-finalDF[!action%>%is.na,]
-write.csv(finalDF,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/save/action0328.csv",row.names = F)
+finalDF<-finalDF[!static_feat%>%is.na,]
+#balance sample
+posSamp<-which(finalDF$label!=-1&finalDF$label!=-2)
+negSamp<-which(finalDF$label==-1)
+predSamp<-which(finalDF$label==-2)
+negSamp<-sample(negSamp,length(posSamp)*2,replace = F)
+trainSamp<-c(posSamp,negSamp)
+testSamp<-sample(trainSamp,length(trainSamp)*0.2,F)
+trainSamp<-trainSamp[!trainSamp%in%testSamp]
+predDF<-finalDF[predSamp,]
+trainDF<- finalDF[trainSamp,]
+testDF<-finalDF[testSamp,]
+write.csv(trainDF,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/save/traindf.csv",row.names = F)
+write.csv(testDF,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/save/testdf.csv",row.names = F)
+write.csv(predDF,file = "C:/Users/Administrator.NBJXUEJUN-LI/Desktop/project/RProj/my code/jdProj/save/preddf.csv",row.names = F)
+ 
+
+
+ 
